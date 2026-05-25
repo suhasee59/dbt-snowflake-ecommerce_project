a034edec70953074f429b8808d4026fe1e89bffb
jezz v <jezzv638@gmail.com>
2026-05-25 08:11:30 +0000

    removed unnecessary

--- models/mart/fact_order.sql
+++ models/mart/fact_order.sql
@@ -12,7 +12,7 @@ with
 
         select *
         from {{ ref("int_order_payment") }}
-        -- incremental filter: only pick up new/updated rows
+
         {% if is_incremental() %}
             where updated_at > (select max(updated_at) from {{ this }})
         {% endif %}
@@ -39,42 +39,29 @@ with
 
 select
 
-    -- identifiers
     o.order_id,
     o.customer_id,
     o.session_id,
     o.payment_id,
     o.transaction_id,
-
-    -- dates
     o.order_date,
     o.processed_at,
-
-    -- order info
     o.order_status,
     o.currency,
     o.device_type,
     o.referrer_source,
-
-    -- shipping
     o.shipping_method,
     o.shipping_cost,
-
-    -- payment
     o.payment_method,
     o.payment_status,
     o.gateway_response_code,
     o.processing_time_sec,
-
-    -- amounts
     o.subtotal,
     o.discount_amount,
     o.tax_amount,
     o.total_amount,
     {{ convert_to_usd('o.total_amount', 'o.currency') }} as total_amount_usd,
     o.payment_amount,
-
-    -- items summary (from order_items)
     i.total_items,
     i.total_quantity,
     i.total_net_amount,
@@ -86,8 +73,6 @@ select
         then true
         else false
     end as is_fully_paid,
-
-    -- metadata
     o.created_at,
     o.updated_at
 
